dbedd6335ac74465e6a9d13588264bc3b2efdbd3
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-04 15:03:47 +0530

    feat: :rocket: Routes redefined

--- controllers/operationsController.js
+++ controllers/operationsController.js
@@ -1,3 +1,11 @@
+const feed = require('../dev-data/data/feed.json');
+
 exports.getFeed = (req, res) => {
-	res.send('Hello');
+	res.status(200).json({
+		status: 'success',
+		results: feed.length,
+		data: {
+			feed,
+		},
+	});
 };
