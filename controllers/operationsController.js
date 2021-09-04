95c55f80d76bc474bc45a2e5f193231e4a149496
Sajal Gupta <guptasajal411@gmail.com>
2021-09-04 18:24:13 +0530

    refactor: :tada: remove unused code, requires and dependencies

--- controllers/operationsController.js
+++ controllers/operationsController.js
@@ -1,8 +1,3 @@
-/* eslint-disable */
-
-const path = require('path');
-const feed = require('../dev-data/data/feed.json');
-const fs = require('fs');
 exports.getFeed = async (req, res) => {
 	const { createClient } = require("@astrajs/collections");
 	const astraClient = await createClient({
@@ -10,19 +5,15 @@ exports.getFeed = async (req, res) => {
 		astraDatabaseRegion: process.env.ASTRA_DB_REGION,
 		applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
 	});
+
 	const postsCollection = astraClient.namespace("posts").collection("postsCollection");
-	// const post = await postsCollection.create({
-	// 	"profilePicture": "https://i.imgur.com/R4ikJd1.jpg",
-	// 	"username": "Andreas Brixen",
-	// 	"postImg": "https://i.imgur.com/l8rN74g.png",
-	// 	"caption": "A good old book.",
-	// 	"points": 80
-	// });
+
 	const posts = await postsCollection.find({});
 	const response = Object.keys(posts).map((key) => ({
 		id: key,
 		...posts[key]
 	}));
+
 	res.json(response);
 };
 
@@ -35,6 +26,7 @@ exports.createFeed = async (req, res) => {
 	});
 
 	const postsCollection = astraClient.namespace("posts").collection("postsCollection");
+
 	const post = await postsCollection.create({
 		"profilePicture": req.body.profilePicture,
 		"username": req.body.username,
@@ -42,4 +34,6 @@ exports.createFeed = async (req, res) => {
 		"caption": req.body.caption,
 		"points": req.body.points
 	});
+
+	res.send("POST request was successfully completed.");
 };
