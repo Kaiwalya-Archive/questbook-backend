f7cb3f68f0ecd6bd00045241fc263f4ca1512717
kaiwalyakoparkar <kaiwalyakoparkar@gmail.com>
2021-09-04 11:37:36 +0530

    feat: :rocket: Seperated the index.js and server.js files

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 
-const port = process.env.PORT;
-
 app.get('/', (req, res) => {
 	res.send('Hello');
 });
 
-app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
-});
+module.exports = app;
