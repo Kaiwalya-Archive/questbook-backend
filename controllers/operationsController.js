0d70c5f5c7179a51e4f324ea896cd82a5609f5ee
Sajal Gupta <guptasajal411@gmail.com>
2021-09-04 18:06:29 +0530

    feat: :rocket: :tada: Connect AstraDB successfully!

--- controllers/operationsController.js
+++ controllers/operationsController.js
@@ -1,15 +1,29 @@
+/* eslint-disable */
+
 const path = require('path');
 const feed = require('../dev-data/data/feed.json');
 const fs = require('fs');
-
-exports.getFeed = (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		results: feed.length,
-		data: {
-			feed,
-		},
+exports.getFeed = async (req, res) => {
+	const { createClient } = require("@astrajs/collections");
+	const astraClient = await createClient({
+		astraDatabaseId: process.env.ASTRA_DB_ID,
+		astraDatabaseRegion: process.env.ASTRA_DB_REGION,
+		applicationToken: process.env.ASTRA_DB_APPLICATION_TOKEN,
 	});
+	const postsCollection = astraClient.namespace("posts").collection("postsCollection");
+	// const post = await postsCollection.create({
+	// 	"profilePicture": "https://i.imgur.com/R4ikJd1.jpg",
+	// 	"username": "Andreas Brixen",
+	// 	"postImg": "https://i.imgur.com/l8rN74g.png",
+	// 	"caption": "A good old book.",
+	// 	"points": 80
+	// });
+	const posts = await postsCollection.find({});
+	const response = Object.keys(posts).map((key) => ({
+		id: key,
+		...posts[key]
+	}));
+	res.json(response);
 };
 
 exports.createFeed = (req, res) => {
